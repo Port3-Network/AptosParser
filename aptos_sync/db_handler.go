6f188d15159331985c743d0d3194478e23fa1e99
wuuuk <1342219013@qq.com>
2022-10-09 03:18:36 +0800

    fix interface to string err

--- aptos_sync/db_handler.go
+++ aptos_sync/db_handler.go
@@ -67,13 +67,6 @@ func handlerHistoryCoin(saver *DbSaver, version, txTime, sequenceNum int64, data
 	var action int64
 	var sender, receiver string = ZeroAddress, ZeroAddress
 
-	if len(data.Payload.TypeArguments) > 0 {
-		resource = data.Payload.TypeArguments[0]
-	}
-	if len(data.Payload.Arguments) > 1 {
-		amount = data.Payload.Arguments[1].(string)
-	}
-
 	for _, event := range data.Events {
 		switch event.Type {
 		case EventDeposit:
@@ -91,6 +84,13 @@ func handlerHistoryCoin(saver *DbSaver, version, txTime, sequenceNum int64, data
 		action = ActionBurn
 	} else {
 		action = ActionTransfer
+	}
+
+	if len(data.Payload.TypeArguments) > 0 {
+		resource = data.Payload.TypeArguments[0]
+	}
+	if len(data.Payload.Arguments) > 1 {
+		amount = data.Payload.Arguments[1].(string)
 	}
 
 	saver.historyCoin = append(saver.historyCoin, &models.HistoryCoin{
